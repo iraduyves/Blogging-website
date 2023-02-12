4789a445641856c74db8a9dd8c930a4610ca1d93
Yves IRADUKUNDA <iradukundayves11@gmail.com>
2023-02-12 12:01:52 +0200

    some changes in backend

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express=require('express');
 const path=require('path');
 
-let initia_path=path.join(__dirname,"public");
+let initial_path=path.join(__dirname,"public");
 
 const path=expres();
 app.use(express.static(initial_path));
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(initia_path,"home.js"));
+    res.sendFile(path.join(initial_path,"home.js"));
 })
 
 app.listen("3000",( )=>{
-    console.log('Listening......')
+    console.log('Listening...........')
 }) 
